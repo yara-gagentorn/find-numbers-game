de4c14abf85051097789412964854ad8fb74c0ff
Yara Gagen-Torn <yaragagen@gmail.com>
2022-07-26 16:29:36 +1200

    random color, font, etc

--- game.js
+++ game.js
@@ -12,27 +12,25 @@
 // generate a new span
 
 const fontFamilies = [
-  "font-family: 'Abril Fatface', cursive",
-  "font-family: 'Alfa Slab One', cursive",
-  "font-family: 'Anton', sans-serif",
-  "font-family: 'Barlow', sans-serif",
-  "font-family: 'Comfortaa', cursive",
-  "font-family: 'Edu VIC WA NT Beginner', cursive",
-  "font-family: 'Fredoka One', cursive",
-  "font-family: 'Lobster', cursive",
-  "font-family: 'Lobster Two', cursive",
-  "font-family: 'Mochiy Pop One', sans-serif",
-  "font-family: 'Orbitron', sans-serif",
-  "font-family: 'Parisienne', cursive",
-  "font-family: 'Quicksand', sans-serif",
-  "font-family: 'Rampart One', cursive",
-  "font-family: 'Shadows Into Light', cursive",
-  "font-family: 'Titan One', cursive",
+  "'Abril Fatface', cursive",
+  "'Alfa Slab One', cursive",
+  "'Anton', sans-serif",
+  "'Barlow', sans-serif",
+  "'Comfortaa', cursive",
+  "'Edu VIC WA NT Beginner', cursive",
+  "'Fredoka One', cursive",
+  "'Lobster', cursive",
+  "'Lobster Two', cursive",
+  "'Mochiy Pop One', sans-serif",
+  "'Orbitron', sans-serif",
+  "'Parisienne', cursive",
+  "'Quicksand', sans-serif",
+  "'Rampart One', cursive",
+  "'Shadows Into Light', cursive",
+  "'Titan One', cursive",
 ]
 
 const fontStyles = ['normal', 'italic']
-
-let newSpan = document.createElement('span')
 
 // random element from array
 function getRandomElement(array) {
@@ -42,40 +40,53 @@ function getRandomElement(array) {
 
 function randomColor(elem) {
   // generate random color
-  elem.style.color = red
+  let someColor = Math.floor(Math.random() * 16777215).toString(16)
+  elem.style.color = '#' + someColor
+  console.log(someColor)
 }
 
 function randomStyleAndWeight(elem) {
-  elem.style.fontStyle = 'italic'
-  elem.style.fontWeight = '900'
+  elem.style.fontStyle = getRandomElement(fontStyles)
+  elem.style.fontWeight = Math.floor(Math.random() * 900)
 }
 
 function randomFont(elem) {
   elem.style.fontFamily = getRandomElement(fontFamilies)
+  console.log('font' + elem.style.fontFamily)
 }
 
-function randomSize(elem) {}
+function randomSize(elem) {
+  elem.style.fontSize = Math.floor(Math.random() * 5) + 'rem'
+  //console.log('size')
+}
 
 // apply all styles
 function assignStyle(elem) {
-  elem.randomColor
-  elem.randomFont
-  elem.randomSize
-  elem.randomStyleAndWeight
+  randomColor(elem)
+  randomFont(elem)
+  randomSize(elem)
+  randomStyleAndWeight(elem)
 }
 
-function generateSpanWithNumbers(howManyNumbers) {
+function generateGameField(howManyNumbers) {
   for (let i = 0; i < howManyNumbers; i++) {
+    console.log('here')
+    let newSpan = document.createElement('span')
+
     newSpan.innerHTML = i
-    newSpan.classList.add('class' + i)
+    //newSpan.classList.add('class' + i)
     if (i % 10 === 0) {
       document
         .getElementById('game-field')
         .appendChild(document.createElement('br'))
     }
     // add div inside game-container
+    assignStyle(newSpan)
+    newSpan.onclick = () => {
+      console.log(i)
+    }
     document.getElementById('game-field').appendChild(newSpan)
   }
 }
 
-generateSpanWithNumbers(100)
+generateGameField(100)
